241f3fbd67562eee77f63f83daa651206ae6a46e
Kalei Trujillo <kalei096@gmail.com>
2018-08-13 15:41:45 -0700

    efactor to use react router dom in preparation for passport functionality

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import axios from 'axios';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Login from './Login.jsx';
 import SignUp from './SignUp.jsx';
 
@@ -7,20 +8,12 @@ class App extends Component {
   constructor(props) {
       super(props);
       this.state = {
-          view: 'login',
           username: '',
           password: ''
       };
-    this.clickHandler = this.clickHandler.bind(this);
     this.handleChange = this.handleChange.bind(this);
   }
 
-  clickHandler (view) {
-
-    this.setState({
-      view: view
-    });
-  }
 
   handleChange (e) {
     this.setState({
@@ -29,31 +22,40 @@ class App extends Component {
   }
 
   render() {
-    //view switcher
-    const {view} = this.state;
-    const landingView = () => {
-      if (view === 'login') {
-        return (
-          <Login username={this.state.username} pw={this.state.password} handleChange={this.handleChange}/>
-        )
-      } else if (view === 'signup') {
-        return  ( 
-          <SignUp username={this.state.username} pw={this.state.password} handleChange={this.handleChange}/>
-        )
-      }
+    const { view, username, password } = this.state;
+
+    const renderLogin = () => {
+      return (
+              <Login username={username} pw={password} handleChange={this.handleChange}/>
+            )
+    }
+
+    const renderSignUp = () => {
+      return  ( 
+              <SignUp username={username} pw={password} handleChange={this.handleChange}/>
+            )
     }
 
 
     return (
-      <div>
+      
+    <Router>
+     <div>
+         
         <h1>CodeQuest Fellowship</h1>
         <nav>
-          <h6 onClick={() =>this.clickHandler('signup')}>Signup</h6>
-          <h6 onClick={() =>this.clickHandler('login')}>Login</h6>
-
+          <ul>
+          <li><Link to="/">Login</Link></li>
+          <li><Link to="/signup">Sign Up</Link></li>
+          </ul>
         </nav>
-        {landingView()}
+        <Switch>
+          <Route exact path="/" render={renderLogin}/>
+          <Route path="/signup" render={renderSignUp}/>
+        </Switch>
       </div>
+    </Router>
+      
     )
   }
 }
