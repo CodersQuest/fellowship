e710a71a706934fded1be456f78aae42c51e6ae3
Kalei Trujillo <kalei096@gmail.com>
2018-08-14 12:03:58 -0700

    create post request for login/signup to server, create server side post to db, client to server connects, submit button works. redirect and db access do not yet work

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 
-const SignUp = ({handleChange}) => (
+const SignUp = ({handleChange, onSubmit}) => (
   <div>
     <h4>Sign Up</h4>
     <label htmlFor="display-name"> Name: </label>
     <input onChange={handleChange} type="text" id="display-name" name="username"  required />
+    <label htmlFor="display-email"> Email:</label>
+    <input onChange={handleChange} type="text" id="display-email"  name="email"  required />
     <label htmlFor="display-password"> Password:</label>
-    <input onChange={handleChange} type="text" id="display-password"  name="password"  required />
-    <button name="submit" type="submit" value="submit-true">
+    <span>
+   <input onChange={handleChange} type="hidden" name="password" required/>
+   <input type="password" className="masked" id="password" autoComplete="off" />
+</span>
+    <button onClick={onSubmit} name="submit" type="submit" value="submit-true">
     Submit
     </button>
   </div>
