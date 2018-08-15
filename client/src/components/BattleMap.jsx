a35714acc2779c2c2ed4ae3be56f64ded59fdfd3
darthsticky <tristancarter@gmail.com>
2018-08-15 13:07:35 -0400

    Canvas Now Renders Properly

--- client/src/components/BattleMap.jsx
+++ client/src/components/BattleMap.jsx
@@ -13,11 +13,14 @@ class BattleMap extends Component {
     }
   }
 
-  componentDidMount() {
+  componentWillMount() {
     this.setState({
       canvasWidth: this.state.boardWidth + this.state.padding + 1,
       canvasHeight: this.state.boardHeight + this.state.padding + 1
-    })
+    });
+  }
+
+  componentDidMount() {
     var canvas = this.refs.canvas;
     var ctx = canvas.getContext("2d");
     const bw = this.state.boardWidth;
@@ -36,24 +39,7 @@ class BattleMap extends Component {
     ctx.strokeStyle = "black";
     ctx.stroke();
   }
-
-  drawBoard(ctx){
-    const bw = this.state.boardWidth;
-    const bh = this.state.boardHeight;
-    const p = this.state.padding;
-
-    for (var x = 0; x <= bw; x += 50) {
-        ctx.moveTo(0.5 + x + p, p);
-        ctx.lineTo(0.5 + x + p, bh + p);
-    }
-
-    for (var x = 0; x <= bh; x += 50) {
-        ctx.moveTo(p, 0.5 + x + p);
-        ctx.lineTo(bw + p, 0.5 + x + p);
-    }
-    ctx.strokeStyle = "#4a4b4c";
-    ctx.stroke();
-  }
+  
   render() {
     return (
       <div id="battleMap">
