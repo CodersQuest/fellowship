f3e08f9949329c8ed51c6cea0392675178f3e9ad
darthsticky <tristancarter@gmail.com>
2018-08-15 12:29:50 -0400

    broken component
    Need to fix still

--- client/src/components/BattleMap.jsx
+++ client/src/components/BattleMap.jsx
@@ -4,14 +4,45 @@ class BattleMap extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      grid: []
+      boardWidth: 1200,
+      boardHeight: 800,
+      padding: 0,
+      canvasWidth: this.state.boardWidth + 1,
+      canvasHeight: this.state.boardHeight + 1
     }
   }
+  componentDidMount() {
+    this.setState({
+      boardWidth: 1200,
+      boardHeight: 800,
+    })
+    const canvas = this.refs.canvas;
+    const ctx = canvas.getContext("2d");
+    this.drawBoard();
+  }
 
+  drawBoard(){
+    var bw = this.state.boardWidth;
+    var bh = this.state.boardHeight;
+    var p = this.state.padding;
+    for (var x = 0; x <= bw; x += 50) {
+        context.moveTo(0.5 + x + p, p);
+        context.lineTo(0.5 + x + p, bh + p);
+    }
+
+
+    for (var x = 0; x <= bh; x += 50) {
+        context.moveTo(p, 0.5 + x + p);
+        context.lineTo(bw + p, 0.5 + x + p);
+    }
+
+    context.strokeStyle = "#4a4b4c";
+    context.stroke();
+  }
   render() {
     return (
       <div id="canvas">
-        CANVAS GOES HERE
+        <canvas ref="canvas" width={this.state.canvasWidth} height={this.state.canvasHeight} />
       </div>
     )
   }
