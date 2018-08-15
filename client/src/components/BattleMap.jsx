933d1a5404e60d201563a060ee14459f98ea3ab3
darthsticky <tristancarter@gmail.com>
2018-08-15 12:54:21 -0400

    Error no longer present
    Canvas however does not draw properly.

--- client/src/components/BattleMap.jsx
+++ client/src/components/BattleMap.jsx
@@ -3,43 +3,46 @@ import React, {Component} from 'react';
 class BattleMap extends Component {
   constructor(props) {
     super(props);
-    
+
     this.state = {
       boardWidth: 1200,
       boardHeight: 800,
       padding: 0,
-      canvasWidth: this.state.boardWidth + 1,
-      canvasHeight: this.state.boardHeight + 1
+      canvasWidth: 0,
+      canvasHeight: 0
     }
   }
 
   componentDidMount() {
-    const canvas = this.refs.canvas;
-    const ctx = canvas.getContext("2d");
-    this.drawBoard();
+    this.setState({
+      canvasWidth: this.state.boardWidth + this.state.padding + 1,
+      canvasHeight: this.state.boardHeight + this.state.padding + 1
+    })
+    var canvas = this.refs.canvas;
+    var context = canvas.getContext("2d");
+    this.drawBoard(context);
   }
 
-  drawBoard(){
-    var bw = this.state.boardWidth;
-    var bh = this.state.boardHeight;
-    var p = this.state.padding;
-    for (var x = 0; x <= bw; x += 50) {
-        context.moveTo(0.5 + x + p, p);
-        context.lineTo(0.5 + x + p, bh + p);
-    }
+  drawBoard(ctx){
+    const bw = this.state.boardWidth;
+    const bh = this.state.boardHeight;
+    const p = this.state.padding;
 
+    for (var x = 0; x <= bw; x += 50) {
+        ctx.moveTo(0.5 + x + p, p);
+        ctx.lineTo(0.5 + x + p, bh + p);
+    }
 
     for (var x = 0; x <= bh; x += 50) {
-        context.moveTo(p, 0.5 + x + p);
-        context.lineTo(bw + p, 0.5 + x + p);
+        ctx.moveTo(p, 0.5 + x + p);
+        ctx.lineTo(bw + p, 0.5 + x + p);
     }
-
-    context.strokeStyle = "#4a4b4c";
-    context.stroke();
+    ctx.strokeStyle = "#4a4b4c";
+    ctx.stroke();
   }
   render() {
     return (
-      <div id="canvas">
+      <div id="battleMap">
         <canvas ref="canvas" width={this.state.canvasWidth} height={this.state.canvasHeight} />
       </div>
     );
