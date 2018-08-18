4670308002c51d5523d5c2948cc0ca4b1ac76bbc
darthsticky <tristancarter@gmail.com>
2018-08-18 16:45:30 -0400

    Updated Socket Events
    Added Comments and several events.

--- client/src/socketClient.js
+++ client/src/socketClient.js
@@ -1,5 +1,41 @@
+import BattleLog from './components/BattleLog';
+
 let socketio = require('socket.io-client');
 
-const socket = socketio.connect('http://localhost:3000');
+export const socket = socketio.connect('http://localhost:3000');
 
 console.log(socket);
+
+// For client side events, we will want to follow a pattern of making a function for the client action
+// within said function we will execute our emit
+
+//! Except for joinGame, leaveGame, and other general events, EVERY game event must pass the roomID along with the other data
+//? a better method may be to simply store the roomID on the socket when the joinGame event occurs.
+// One of our first events to emit would be a connect event that passes the user data back to the server
+// said event should update our players object, but also store a userID on the socket
+// if we properly persist this, we may be able to consistently connect players to the proper room no matter their socketID
+
+//emit a custom connect event to update socket with user info
+// listens for a response to that event listen for newPlayer
+
+// joinGame triggers on gameRoom mount
+export const joinGame = roomData => {
+  // this emit expects data to be a game room name or id
+  socket.emit('joinGame', data);
+}
+// diceRoll 
+export const diceRoll = rollMsg => {
+  // this emits a single dice roll message
+  // said message should be constructed from the userName
+  // the dice roll, and other relevant info.
+  // can simply be an object sotring those values and the message can be constructed server side
+  socket.emit('diceRoll', rollMsg);
+}
+// updateCurrentPlayers event
+
+// updateBattleLog event
+export const updateBattleLog = () => {
+  socket.on('updateBattleLog', log => {
+    this.setState({battleLog: log});
+  });
+}
--- server/socket.js
+++ server/socket.js
@@ -1,6 +1,15 @@
 // handle our socket events and more here.
 var socketIo = require("socket.io");
 const sharedSession = require('express-socket.io-session');
+//! We will want to store a list of games, rooms, and players in memory
+// These lists should take the form of objects for constant time lookup
+// Our players list should update upon socket connection,
+// games and rooms should only update based on a joinRoom event
+// a game instance should only be saved to the game object if it doesn't currently exist in the game object
+// otherwise, we should pull the exisiting game from our game object to send back
+// rooms should be updated in the same manner as the games.
+// it's a good idea to track these things separately.
+var games = {}, players = {}, rooms = {};
 
 // declare module constructor that is passed the http server to bind to
 module.exports = function(server, session) {
@@ -15,5 +24,8 @@ module.exports = function(server, session) {
     socket.on("send message", function(data) {
       io.emit("new message", data);
     });
+    socket.on('joinGame', socket => {
+      //! attach roomID to the socket
+    })
   });
 };
