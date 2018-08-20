29395340c510eec9c8980dc9e7c7c39d2c3260b2
Vernon E. Neilly, II <28078313+vneilly2@users.noreply.github.com>
2018-08-20 13:28:23 -0700

    Updated list item JSX layout and prop references

--- client/src/components/GamesListItem.jsx
+++ client/src/components/GamesListItem.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import styles from '../styles/App.css'
-
+const listItemStyle = {
+  width: 75,
+  height: 75
+}
 const GamesListItem = ({game}) => {
 
   return (
@@ -8,17 +11,20 @@ const GamesListItem = ({game}) => {
       <ul>
         <li>
         <div className="wrapper">
-          <div className="gameImage">
-            <img src={game.imageUrL}/>
-          </div>
-          <div className="nested">
-            <div className="gameName">
-              {game.gameName}
-            </div>
-            <div className="gameDescription">
-              {game.description}
-            </div>
-          </div>
+          <span className="gameImage">
+            <img style={listItemStyle}
+            src={game.gameImg}/>
+          </span>
+          <span className="gameName">
+            {game.gameName}
+          </span>
+          <span className="gameId">
+            {game.gameId}
+          </span>
+          <span className="gameDescription">
+            {game.gameDesc}
+          </span>
+          <span><button type='button'>Join Game</button></span>
         </div>
         </li>
       </ul>
