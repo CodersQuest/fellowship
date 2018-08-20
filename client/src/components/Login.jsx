300fd586df92e132a696eff37c9475f03e34964d
tonsojun <josephjun@gmail.com>
2018-08-20 10:25:55 -0700

    Updated Login styling

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,40 +1,41 @@
 import React from 'react';
 
-
 const Login = ({handleChange, onSubmit, viewChange}) => (
-  <div className="columns is-mobile is-centered">
-    <div className="column is-half">
-      <p onClick={()=> viewChange('/signup')}>Sign Up</p>
 
-      <h3>Login</h3>
+    <div className="container">
+      <div className="hero">
+        <p onClick={()=> viewChange('/signup')}>Sign Up</p>
 
-      <form >
+        <h3>Login</h3>
 
-        <div className="field">
-          <label classsName="label"> username </label>
-          <div className="control has-icons-left has-icons-right">
-            <input className="input is-success" type="text" placeholder="username" onChange={handleChange} type="text" id="display-name" name="username"  required/>
-            <span className="icon is-small is-left"/>
-              <i className="fas fa-user"/>
+        <form >
+
+          <div className="field">
+            <label classsName="label"> username </label>
+            <div className="control has-icons-left has-icons-right">
+              <input className="input is-success" type="text" placeholder="username" onChange={handleChange} type="text" id="display-name" name="username"  required/>
+              <span className="icon is-small is-left"/>
+                <i className="fas fa-user"/>
+            </div>
           </div>
-        </div>
 
-        <div className="field">
-          <label classsName="label"> password </label>
-          <div className="control has-icons-left has-icons-right">
-            <input className="input is-success" type="text" placeholder="password" onChange={handleChange} type="text" id="display-name" name="password"  required/>
-            <span className="icon is-small is-left"/>
-              <i className="fas fa-user"/>
+          <div className="field">
+            <label classsName="label"> password </label>
+            <div className="control has-icons-left has-icons-right">
+              <input className="input is-success" type="text" placeholder="password" onChange={handleChange} type="text" id="display-name" name="password"  required/>
+              <span className="icon is-small is-left"/>
+                <i className="fas fa-user"/>
+            </div>
           </div>
-        </div>
 
-        <button className="button is-fullwidth" onClick={onSubmit} name="submit" type="submit" value="submit-true">
-          Login
-        </button>
-        
-      </form>
+          <button className="button is-fullwidth" onClick={onSubmit} name="submit" type="submit" value="submit-true">
+            Login
+          </button>
+          
+        </form>
+      </div>
     </div>
-  </div>  
+
 )
 
 export default Login;
