4ef27385e2b25c639195355b9d5b292784013972
Kalei Trujillo <kalei096@gmail.com>
2018-08-20 16:40:36 -0700

    to rebase

--- client/src/components/TokenLayer.jsx
+++ client/src/components/TokenLayer.jsx
@@ -15,70 +15,48 @@ class TokenLayer extends Component {
     this.handleDragEnter = this.handleDragEnter.bind(this);
     this.handleDragLeave = this.handleDragLeave.bind(this);
     this.handleDrop = this.handleDrop.bind(this);
-    //may have to redo dimensions to match dynamic render of under-canvas layer
   }
-  handleDragEnter(e) {
-    // this / e.target is the current hover target.
-    // e.target.style.border = 'dotted'
 
+  handleDragEnter(e) {
     e.target.classList.add('over');
   }
   
   handleDragLeave(e) {
-    // e.target.style.border = 'solid'
-
     e.target.classList.remove('over');  // this / e.target is previous target element.
   }
-
-
-
-  //dragenter for target area for canvas
-  //dragover for target area for canvas
-  //dragleave for canvas
-  //handledrop /actual createion of copy for canvas
 
   handleDragOver(e) {
     if (e.preventDefault) {
       e.preventDefault(); // Necessary. Allows us to drop.
     }
-    // e.dataTransfer.dropEffect = 'copy';  // See the section on the DataTransfer object.
-    // console.log(e.dataTransfer.files, 'data')
-
     return false;
   }
 
    handleDrop (e) {
     // this / e.target is current target element.
-  
     if (e.stopPropagation) {
       e.stopPropagation(); // stops the browser from redirecting.
     }
-  
       //create image object from image dragged
       //add to canvas
     var img = document.querySelector('.target-image');
     // console.log('event: ', e);
-
     var newImage = new fabric.Image(img, {
         width: img.width,
         height: img.height,
-        // Set the center of the new object based on the event coordinates relative
-        // to the canvas container.
-        left: e.layerX,
-        top: e.layerY
+        left: e.layerX,//round to nearest 50 value
+        top: e.layerY//round to nearest 50 value
     });
     this.state.canvas.add(newImage);
     return false;
   }
   
+  componentDidMount() { 
+    var c = new fabric.Canvas('canvas-top');
 
-  componentDidMount() {
-    
-    var c = new fabric.StaticCanvas('canvas-top');
     this.setState({
       canvas: c
     })
-
     var t = document.querySelectorAll('#canvas-top');
     t.forEach(tokenTarget => {
       tokenTarget.addEventListener('dragenter', this.handleDragEnter, false)
@@ -86,8 +64,6 @@ class TokenLayer extends Component {
       tokenTarget.addEventListener('dragleave', this.handleDragLeave, false)
       tokenTarget.addEventListener('drop', this.handleDrop, false)
     })
-
-
   }
 
   render () {
@@ -98,12 +74,8 @@ class TokenLayer extends Component {
         height={this.state.boardHeight}
         ></canvas>  
         </div>
-
-    )
-    
+    )   
   }
-
-
 }
 
 export default TokenLayer;
--- client/src/components/TokenTemplateList.jsx
+++ client/src/components/TokenTemplateList.jsx
@@ -1,37 +1,31 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import TokenTemplateListItem from './TokenTemplateListItem.jsx';
 
 
 class TokenTemplateList extends Component {
   constructor(props) {
     super(props);
-    this.state ={
+    this.state = {
       state: null
-      }
+    }
     this.handleDragStart = this.handleDragStart.bind(this);
     this.handleDragEnd = this.handleDragEnd.bind(this);
   }
 
   handleDragStart(e) {
-    e.target.style.opacity = '0.4';  // this / e.target is the source node.
-    // dragSrcEl = e.target;
+    e.target.style.opacity = '0.4'; 
     e.target.classList.add('target-image')
     e.dataTransfer.effectAllowed = 'move';
     e.dataTransfer.setData('text/html', this.innerHTML);
   }
 
   handleDragEnd(e) {
-    // this/e.target is the source node.
-    // var t = document.querySelectorAll(".token-item")
-    // t.forEach(token => {
-    // token.classList.remove('over');
-    // });
     e.target.style.opacity = '1';
     e.target.classList.remove('target-image')
 
   }
 
-  componentDidMount () {
+  componentDidMount() {
     var t = document.querySelectorAll(".token-item")
     t.forEach(token => {
       token.addEventListener('dragstart', this.handleDragStart, false);
@@ -39,20 +33,18 @@ class TokenTemplateList extends Component {
     })
   }
 
-  render () {
-      return (
-          <div id="tokenlist">
-          {this.props.tokenImages.map(url =>
-          
-          <TokenTemplateListItem 
-          // handleDragOver={this.handleDragOver}
-          // handleDragStart={this.handleDragStart}
-          imgUrl={url}/>
-        
-        
+  render() {
+    return (
+      <div id="tokenlist">
+        {this.props.tokenImages.map(url =>
+
+          <TokenTemplateListItem
+            imgUrl={url} />
+
+
         )}
-          </div>
-      )
+      </div>
+    )
   }
 
 }
