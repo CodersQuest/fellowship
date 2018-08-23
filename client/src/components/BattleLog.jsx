d4a186d20443f327fb808608984e2b6c16dbc76d
Vernon E. Neilly, II <28078313+vneilly2@users.noreply.github.com>
2018-08-23 00:56:04 -0700

    update UI layout and CSS for BattleLog

--- client/src/components/BattleLog.jsx
+++ client/src/components/BattleLog.jsx
@@ -1,17 +1,38 @@
 import React from 'react';
+import 'bulma/css/bulma.css';
 
 // Component to render battle log messages.
 // This includes Dice Rolls, and eventually player messages and spells/skills
 class BattleLog extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      logStorage:[],
+    }
+    this.handleDisplayLog = this.handleDisplayLog.bind(this);
+  }
+
+  handleDisplayLog(logArr) {
+
   }
 
   render() {
     return (
       <div id="battleLog">
-        <h1>BATTLE LOG</h1>
-      </div>
+        <div className='columns'>
+          <div className='column'>
+            <div className='level-item has-text-centered custom-bg'>
+              <h1 className='heading'>BATTLE LOG</h1>
+            </div>
+
+            <div className='log-display-wrapper'>
+              <div className='log-entry-view'>
+                {/* static dom element to be replaced by Component */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> // end #battleLog
     )
   }
 }
