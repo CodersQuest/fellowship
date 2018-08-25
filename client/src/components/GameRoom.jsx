0e347b8d1629974d1ae6ebefefc068b22c98b337
darthsticky <tristancarter@gmail.com>
2018-08-25 19:34:30 -0400

    Fixed most linting in gameroom updated eslintrc

--- client/src/components/GameRoom.jsx
+++ client/src/components/GameRoom.jsx
@@ -1,15 +1,32 @@
-import React, { Component, Fragment } from 'react';
-import { Redirect } from 'react-router-dom';
+import React, {Component, Fragment} from 'react';
+import {Redirect} from 'react-router-dom';
 import BattleMap from './BattleMap.jsx';
-import BattleLog from "./BattleLog.jsx";
-import GameProfiles from "./GameProfiles.jsx";
-import GameOptions from "./GameOptions.jsx";
-import DiceTray from "./DiceTray.jsx";
-import { joinGame, leaveGame, diceRoll, addToken, removeToken, moveToken, playerConnect, socket } from '../socketClient.js';
+import BattleLog from './BattleLog.jsx';
+import GameProfiles from './GameProfiles.jsx';
+import GameOptions from './GameOptions.jsx';
+import DiceTray from './DiceTray.jsx';
+import {
+  joinGame,
+  leaveGame,
+  diceRoll,
+  addToken,
+  removeToken,
+  moveToken,
+  playerConnect,
+  socket
+} from '../socketClient.js';
 import TokenTemplateList from './TokenTemplateList.jsx';
-import {eevee, ninetails, clefairy, lugia, defaultGameAvatar} from '../images/imageData.js';
- 
+import {eevee, ninetails, clefairy, lugia} from '../images/imageData.js';
+/**
+ * Adds two numbers together.
+ * @param {int} num1 The first number.
+ * @param {int} num2 The second number.
+ */
 class GameRoom extends Component {
+  /**
+ * Props from App.
+ * @param {object} props object containing methods and state from app.
+ */
   constructor(props) {
     super(props);
     this.state = {
@@ -18,106 +35,120 @@ class GameRoom extends Component {
       tokens: [],
       log: [],
       tokenImages: [eevee, ninetails, clefairy, lugia],
-      clearTokens: false
-    }
+      clearTokens: false,
+    };
     this.rollDice = this.rollDice.bind(this);
     this.onClear = this.onClear.bind(this);
     this.handleLeaveGame = this.handleLeaveGame.bind(this);
   }
-  
-
+/**
+ * React standard mount.
+ * Initializes all socket event listeners for game setup.
+ */
   componentDidMount() {
     playerConnect(this.props.currentUser);
-    socket.on('newPlayer', data => {
+    socket.on('newPlayer', (data) => {
       joinGame(this.props.currentGame);
     });
-    socket.on('gameStatusUpdated', gameData => {
-      // data.logs, data.players, data.tokens 
+    socket.on('gameStatusUpdated', (gameData) => {
+      // data.logs, data.players, data.tokens
       this.setState({
         players: gameData.players,
         tokens: gameData.tokens,
-        log: gameData.logs
+        log: gameData.logs,
       });
     });
-    socket.on('updateLog', logData => {
+    socket.on('updateLog', (logData) => {
       this.setState({
-        log: logData
+        log: logData,
       });
     });
-    socket.on('playerLeft', playerData => {
+    socket.on('playerLeft', (playerData) => {
       this.setState({
-        players: playerData
+        players: playerData,
       });
     });
   }
-
+/**
+ * makes a random dice roll inclusive to the max.
+ * sends data from roll and player in socket emit.
+ * @param {int} value Number representing specific die clicked.
+ */
   rollDice(value) {
     const roll = Math.floor(Math.random() * (value - 1 + 1) + 1);
     const user = this.props.currentUser.username;
-    //! TODO: Add roll context to this later. As well as Bonus modifier.
+    // ! TODO: Add roll context to this later. As well as Bonus modifier.
     diceRoll({
       player: user,
       roll: roll,
-      max: value
+      max: value,
     });
   }
-
+/**
+ * Function to Run through click handler for leaving games.
+ * No params needed. Will fire socket leaveGame event.
+ */
   handleLeaveGame() {
     leaveGame();
   }
-
-  onClear () {
-    var c = document.getElementById('canvas').fabric;
-    c.getObjects().map(obj => {
+/**
+ * Clears Fabric Canvas.
+ */
+  onClear() {
+    const c = document.getElementById('canvas').fabric;
+    c.getObjects().map((obj) => {
       if (obj.selectable === true) {
-        c.remove(obj)
+        c.remove(obj);
       }
     });
     c.renderAll.bind(c);
   }
-  
+/**
+ * React Render
+ * @return {renderdom} renders conditional component dependant on router.
+ */
   render() {
     const isLoggedIn = this.props.isLoggedIn;
-    
-    if (this.props.currentUser === null)  {
+
+    if (this.props.currentUser === null) {
       return (
-        <Redirect 
+        <Redirect
           to={{
-            pathname: "/login",
+            pathname: '/login',
             state: {
-              from: this.props.location.pathname
-            }
+              from: this.props.location.pathname,
+            },
           }}
         />
-      )
+      );
     }
 
-    if ( isLoggedIn && (this.props.currentGame).hasOwnProperty('gameId') ) {  
+    if ( isLoggedIn && (this.props.currentGame).hasOwnProperty('gameId') ) {
       return (
         <Fragment>
           <div id="gameContainer">
-            <TokenTemplateList 
-            onClear={this.onClear} 
+            <TokenTemplateList
+            onClear={this.onClear}
             tokenImages={this.state.tokenImages} />
-            <BattleMap clearTokens={this.state.clearTokens} />   
+            <BattleMap clearTokens={this.state.clearTokens} />
             <BattleLog currentLog={this.state.log} {...this.props} />
             <GameProfiles players={this.state.players} />
             <GameOptions leaveGame={this.handleLeaveGame} game={this.props.currentGame} />
             <DiceTray rollDice={this.rollDice} />
           </div>
         </Fragment>
-      )
+      );
     } else {
       return (
-        <Redirect 
+        <Redirect
           to={{
-            pathname: "/",
+            pathname: '/',
             state: {
-              from: this.props.location.pathname
-            }
+              from: this.props.location.pathname,
+            },
           }}
         />
-      )
+      );
     }
   }
 }
