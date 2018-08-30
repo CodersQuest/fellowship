226d10dbe007512e7d5d09b90a84a5b5ce38e11f
Tristan Carter <tristancarter@gmail.com>
2018-08-30 13:16:42 -0400

    added icons to battle log for dice rolls

--- client/src/components/BattleLogEntry.jsx
+++ client/src/components/BattleLogEntry.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {ability, damage, initiative, magic, melee, ranged, skill, saving} from '../images/logIcons/logIcons.js';
 const parseTime = (date) => {
   let rightNow = Date.now();
   let milliTime = (rightNow - date) / 1000;
@@ -13,6 +14,56 @@ const parseTime = (date) => {
   }
 }
 
+const battleIcons = {
+  'Ranged Attack': {
+    text: 'ranged attack',
+    url: ranged,
+  },
+  'Melee Attack': {
+    text: 'melee attack',
+    url: melee,
+  },
+  'Spell Attack': {
+    text: 'magic attack',
+    url: magic,
+  },
+  'Damage': {
+    text: 'damage',
+    url: damage,
+  },
+  'Skill Check': {
+    text: 'skill check',
+    url: skill,
+  },
+  'Ability Check': {
+    text: 'ability check',
+    url: ability,
+  },
+  'Initiative': {
+    text: 'initiative',
+    url: initiative,
+  },
+  'Saving Throw': {
+    text: 'saving throw',
+    url: saving,
+  },
+};
+
+const renderIcon = (iconType) => {
+  const iconStyle = {
+    width: 30,
+    height: 30,
+  };
+  if (battleIcons[iconType]) {
+    return (
+      <span className='logIcons'>
+        <img src={battleIcons[iconType].url} style={iconStyle}/>
+        <div>{battleIcons[iconType].text}</div>
+      </span>
+    );
+  }
+};
+
 const BattleLogEntry = ({logentry,}) => {
   return logentry.type ?
   (<li className='logentry'>
@@ -21,7 +72,11 @@ const BattleLogEntry = ({logentry,}) => {
   </li>) :
   (<li className='logentry'>
     <div className="player">{logentry.player} : </div>
-    Rolled <span className='rollSize'>{logentry.roll}</span> out of {logentry.max} with a + {logentry.bonus} {logentry.context === 'none' ? null : 'for ' + logentry.context}
+    Rolled <span className='rollSize'>{logentry.roll}</span> out of <span className='rollSize'>{logentry.max}</span>
+    {logentry.bonus === 0 ? null : ' with a + '}
+    <span className='rollSize'>{logentry.bonus === 0 ? null : logentry.bonus}</span>
+    {logentry.context === 'none' ? null : ' for  '}
+    {logentry.context === 'none' ? null : renderIcon(logentry.context)}
   </li>);
 };
 
---
+++ client/src/images/logIcons/logIcons.js
@@ -0,0 +1,8 @@
+module.exports.ability = require('./ability.png');
+module.exports.damage = require('./damage.png');
+module.exports.initiative = require('./initiative.png');
+module.exports.magic = require('./magic.png');
+module.exports.melee = require('./melee.png');
+module.exports.ranged = require('./ranged.png');
+module.exports.saving = require('./saving.png');
+module.exports.skill = require('./skill.png');
